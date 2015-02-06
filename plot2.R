d1fe71dea414f7aa4772ca9d4078e83d26a5813d
matkins1 <michael.atkins1@gmail.com>
2015-02-06 17:58:06 -0600

    Exploratory Data Analysis Coursera Proejct #1
    Four scripts and output .png images for Coursera proejct

---
+++ plot2.R
@@ -0,0 +1,17 @@
+install.packages("lubridate") ## install lubridate
+library(lubridate) ## load lubridate package
+
+## transform raw data for analysis
+rawdata <- read.table("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE, na.strings = "N/A", header = TRUE) ## reads raw data
+rawdatasubset <- subset(rawdata, Date == "1/2/2007" | Date == "2/2/2007") ## subset 1/2/2007 & 2/2/2007 data
+rawdatasubset$DateTime  <- paste (rawdatasubset$Date, rawdatasubset$Time, sep = " ", collapse = NULL) ## create DateTime column
+rawdatasubset$DateTime <- parse_date_time(rawdatasubset$DateTime, order = "%d/%m/%y %H:%M:%S") ## parse DateTime characters to POSIXct
+rawdatasubset$Global_active_power <- as.numeric(rawdatasubset$Global_active_power) ## convert to numeric
+rawdatasubset$Global_reactive_power <- as.numeric(rawdatasubset$Global_reactive_power) ## convert to numeric
+rawdatasubset$Voltage <- as.numeric(rawdatasubset$Voltage) ## convert to numeric
+rawdatasubset$Sub_metering_1 <- as.numeric(rawdatasubset$Sub_metering_1) ## convert to numeric
+rawdatasubset$Sub_metering_2 <- as.numeric(rawdatasubset$Sub_metering_2) ## convert to numeric
+
+png(file = "plot2.png", width = 480, height = 480) ## write to .png
+plot(rawdatasubset$DateTime, rawdatasubset$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l") ## Plot 2
+dev.off() ## turn of graphics device
